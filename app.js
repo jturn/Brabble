2aa171ce8c49665e64e76253a12fdbd78c0c8437
Josh Turner <talktojosh@gmail.com>
2015-06-15 16:32:33 -0700

    Adjust randomLetter to make vowels and some consonants more common.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var alphabet = ['A', 'A', 'A', 'B', 'B', 'C', 'C', 'D',
+ 'D', 'E', 'E', 'E', 'F', 'G', 'H', 'I', 'I', 'J', 'K', 'L',
+ 'L', 'M', 'M', 'N', 'N', 'O', 'O', 'O', 'O', 'P', 'Q', 'R', 'R', 'S',
+ 'S', 'T', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var randomLetter = function(){
 	return alphabet[Math.floor(Math.random() * 26)];
