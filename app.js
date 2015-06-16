585ab4db9bdb4e0a38a4ff1ac402363bf583a8e9
Josh Turner <talktojosh@gmail.com>
2015-06-16 00:02:03 -0700

    Added start button and timer functionality.

--- app.js
+++ app.js
@@ -1,58 +1,81 @@
-var alphabet = ['A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D',
+var alphabet = ['A', 'A', 'A', 'B', 'B', 'C', 'C', 'D',
  'D', 'E', 'E', 'E', 'F', 'G', 'H', 'I', 'I', 'I', 'J', 'K', 'L',
  'L', 'M', 'M', 'N', 'N', 'O', 'O', 'O', 'O', 'P', 'Q', 'R', 'R', 'S',
  'S', 'T', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var randomLetter = function(){
-	return alphabet[Math.floor(Math.random() * 45)];
+	return alphabet[Math.floor(Math.random() * 44)];
 };
 
-//Eventually this will all happen on new game intialization
+var newGame = function(){
+
+	var newTiles = function(){
+		$('.tile').each(function(){
+			var letter = randomLetter();
+			this.innerHTML = letter;
+			$(this).data('value', letter);
+		});
+	};
+	
+	$('.tile').draggable({
+	  containment: '#main',
+	  cursor: 'move',
+	  revert: 'invalid',
+	  appendTo: 'td'
+	});
+
+	$('td').droppable({
+			accept: '.tile',
+			drop: function(event, ui){
+	      var letter = $(ui.draggable).data('value');
+		    $(this).data('value', letter);
+	  }
+	});
+
+  newTiles();
+};
+
+ 
+
+var startGame = function(){
+	var count = 5;
+	var interval;
+
+	var countdownTimer = function(){
+		interval = setInterval(countdown, 1000);
+	};
+
+	var countdown = function(){
+	  if (count > 0){
+	  	console.log('inside counter loop');
+	    count--;
+	    $('.timer').text('0:' + count);
+	  } else {
+        clearInterval(interval);
+        endGame();
+      }
+	};
+
+	countdownTimer();
+
+};
+
+var endGame = function(){
+	alert("Total words: 0!");
+  count = 60;
+  $('.timer').text("1:00");
+};
+
+// var checkWords = function(){
+
+// };
+
 
 $(document).ready(function(){
 
-var newTiles = function(){
-	$('.tile').each(function(){
-		var letter = randomLetter();
-		this.innerHTML = letter;
-		$(this).data('value', letter);
-	});
-};
-		
-$('.tile').draggable({
-  containment: '#main',
-  cursor: 'move',
-  revert: 'invalid',
-  appendTo: 'td'
+$('.start').on('click', function(){
+	newGame();
+	startGame();
 });
-
-$('td').droppable({
-		accept: '.tile',
-		drop: function(event, ui){
-  var letter = $(ui.draggable).data('value');
-  console.log('FIRING!!!');
-	$(this).data('value', letter);
-  }
-
-});
-
-
-
-
-
-// var letterDrop = function(event, ui){
-	
-// 	var letter = $(this).data('value');
-// 	$(this).append($(ui.draggable));
-
-// 	ui.draggable.position({
-// 	 of: $(this), 
-// 	 my: 'left top', 
-// 	 at: 'left top'
-// 	});
-//   $(this).droppable('disable');
-
-
-newTiles();
 
 });
